4f2276073194e60af8e9fceccb1aab545a2daffd
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2023-02-17 14:24:28 +0100

    Added small cypress test that visits main page and clicks a button.

---
+++ liquibase/changesets/00001-admin-password.sql
@@ -0,0 +1,3 @@
+-- set the admin password to the following:
+-- ,d%6WU{lNy4>|uw&qUyB
+update users set password = '993ecee32586eef6ad41c99c2a9405dfa1e5806e872f813c4a2231ba62fb02ec8639ab86dde6f143' where name = 'admin';
