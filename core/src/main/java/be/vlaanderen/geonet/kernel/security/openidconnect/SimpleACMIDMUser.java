b5d8b01423958d1b9b62b052de3edd04c6ea03f3
CMath04 <chaussier.mathieu@gmail.com>
2023-03-15 11:53:33 +0100

    Draft

---
+++ core/src/main/java/be/vlaanderen/geonet/kernel/security/openidconnect/SimpleACMIDMUser.java
@@ -0,0 +1,20 @@
+package be.vlaanderen.geonet.kernel.security.openidconnect;
+
+import org.fao.geonet.kernel.security.openidconnect.OIDCConfiguration;
+import org.fao.geonet.kernel.security.openidconnect.OIDCRoleProcessor;
+import org.fao.geonet.kernel.security.openidconnect.SimpleOidcUser;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+
+import java.util.Map;
+
+public class SimpleACMIDMUser extends SimpleOidcUser {
+    SimpleACMIDMUser(OIDCConfiguration oidcConfiguration, OIDCRoleProcessor oidcRoleProcessor, OidcIdToken idToken, Map userAttributes) throws Exception {
+        super(oidcConfiguration, oidcRoleProcessor, idToken, userAttributes);
+        super.setUsername(super.getEmail());
+    }
+
+    SimpleACMIDMUser(OIDCConfiguration oidcConfiguration, OIDCRoleProcessor oidcRoleProcessor, Map attributes) throws Exception {
+        super(oidcConfiguration, oidcRoleProcessor, attributes);
+        super.setUsername(super.getEmail());
+    }
+}
