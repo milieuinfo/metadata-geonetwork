db206f9c8de3dd7bfb5514a9d651afa0597ca37b
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2023-03-16 11:16:50 +0100

    Added liquibase reset script.

---
+++ liquibase/reset-db.sh
@@ -0,0 +1,8 @@
+# run this script to start from a fresh database
+# this removes all data contained within and reruns the liquibase changesets
+POSTGRESCONTAINER=metadatageonetwork-database-1
+POSTGRESUSER=geonetwork
+docker exec -it $POSTGRESCONTAINER psql -U $POSTGRESUSER -c "drop schema if exists public, liquibase"
+docker exec -it $POSTGRESCONTAINER psql -U $POSTGRESUSER -c "create schema public"
+docker exec -it $POSTGRESCONTAINER psql -U $POSTGRESUSER -c "create schema liquibase"
+mvn liquibase:update
