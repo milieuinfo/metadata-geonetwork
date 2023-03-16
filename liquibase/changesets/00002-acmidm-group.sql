e417bb987768e166286eca125f5af05a96825c11
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2023-03-16 16:27:00 +0100

    Cleaned up the liquibase changesets.

--- liquibase/changesets/00002-acmidm-group.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE public."groups" ADD orgcode varchar NULL;
-COMMENT ON COLUMN public."groups".orgcode IS 'the org code for this group, as returned by acm/idm';
-ALTER TABLE public."groups" ADD ismdc bool NULL;
-COMMENT ON COLUMN public."groups".ismdc IS 'whether this group is a metadatacenter group or not';
-
-ALTER TABLE public."groups" ALTER COLUMN "name" TYPE varchar(255) USING "name"::varchar;
-
-ALTER TABLE public."groups" ADD CONSTRAINT groups_unq_orgcode_mdc UNIQUE (ismdc,orgcode);
