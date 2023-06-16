41fb27aebe80c3d67ebd4ed14f871c993c8bda8f
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2023-06-16 14:30:34 +0200

    Saw nullpointers in beta, potential solution

--- core/src/main/java/org/fao/geonet/web/LocaleRedirects.java
+++ core/src/main/java/org/fao/geonet/web/LocaleRedirects.java
@@ -147,7 +147,7 @@ public class LocaleRedirects {
                                            final String langHeader) throws ResourceNotFoundException {
         String lang = lang(langParam, langCookie, langHeader);
 
-        if (checkPortalExist(portal, !accept.startsWith(ACCEPT_VALUE))) {
+        if (checkPortalExist(portal, accept!=null && !accept.startsWith(ACCEPT_VALUE))) {
             return redirectURL(createServiceUrl(request, _homeRedirectUrl, lang, portal));
         } else {
             if (sourceRepository.findByType(SourceType.subportal, null).size() == 0) {
