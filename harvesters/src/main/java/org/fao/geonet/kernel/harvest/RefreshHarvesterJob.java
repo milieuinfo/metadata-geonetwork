e5fb6130ade317d25e93e70dc6238d753e85ec9e
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2023-08-31 10:14:49 +0200

    wip

---
+++ harvesters/src/main/java/org/fao/geonet/kernel/harvest/RefreshHarvesterJob.java
@@ -0,0 +1,23 @@
+package org.fao.geonet.kernel.harvest;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.SchedulerContext;
+import org.quartz.SchedulerException;
+
+public class RefreshHarvesterJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        try {
+            SchedulerContext schedulerContext = context.getScheduler().getContext();
+            HarvestManagerImpl harvestManager = (HarvestManagerImpl) schedulerContext.get("harvest-manager");
+            if (harvestManager != null) {
+                harvestManager.refreshHarvesters();
+            }
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+    }
+}
