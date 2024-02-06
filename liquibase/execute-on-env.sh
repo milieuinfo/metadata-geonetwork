e2556bc3240d9a43ee29e23c134bdd69b7826017
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2024-02-06 13:19:46 +0000

    Merged PR 32146: Migration updates - prep for GN3>4
    Updating docs, migration steps, ...
    
    Related work items: #178751, #186360, #186833, #186965

--- liquibase/execute-on-env.sh
+++ liquibase/execute-on-env.sh
@@ -12,7 +12,7 @@ Make sure you have the following environment variables set to satisfy the change
 - LIQUIBASE_MAILPIT_PORT
 "
 
-valid_namespaces=("dev" "bet")
+valid_namespaces=("dev" "bet" "prd")
 while getopts ':hn:' option; do
   case "$option" in
   h)
@@ -69,13 +69,20 @@ if [ -z "$LIQUIBASE_MAILPIT_HOST" ]; then
   exit 1
 fi
 
+# set the cluster
+if [ "$namespace" == "prd" ]; then
+  cluster=prd-md-cluster-aks
+else
+  cluster=pre-md-cluster-aks
+fi
+
 # run the liquibase update
 deployment=deployment/postgres-geonetwork
 localport=54345
 targetport=5432
 
 # set up port forward in background
-kubectl --namespace "$namespace" port-forward deployment/postgres-geonetwork $localport:$targetport &
+kubectl --cluster "$cluster" --namespace "$namespace" port-forward deployment/postgres-geonetwork $localport:$targetport &
 # wait till port forward is running
 sleep 5
 
--- liquibase/port-forward-db.sh
+++ liquibase/port-forward-db.sh
@@ -3,8 +3,9 @@ DBDEPLOYMENT=deployment/postgres-geonetwork
 DBPORT=5432
 
 # Push commands in the background, when the script exits, the commands will exit too
-kubectl --namespace "dev" port-forward $DBDEPLOYMENT 5433:$DBPORT & \
-kubectl --namespace "bet" port-forward $DBDEPLOYMENT 5434:$DBPORT & \
+kubectl --cluster pre-md-cluster-aks --namespace dev port-forward $DBDEPLOYMENT 5433:$DBPORT & \
+kubectl --cluster pre-md-cluster-aks --namespace bet port-forward $DBDEPLOYMENT 5434:$DBPORT & \
+kubectl --cluster prd-md-cluster-aks --namespace prd port-forward $DBDEPLOYMENT 5435:$DBPORT & \
 
 # Wait till we're done
 echo "Press CTRL-C to stop port forwarding and exit the script"
