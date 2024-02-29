ce40a34d7853721b12eeb796323e83d6ae637c59
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2024-02-29 11:28:43 +0100

    cleanup

--- vlaanderen/scripts/public.changelog.sh
+++ vlaanderen/scripts/public.changelog.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+# variables
 changesetFolder=../../liquibase/changesets
 tempMdFileName=public.changelog.md
-tempMdFile=/tmp/$tempMdFileName
 changelogFile=../CHANGELOG.md
 publicChangeLogFileName=public.changelog.html
-publicChangeLogFile=$changesetFolder/$publicChangeLogFileName
 
 # test we have the prerequisite binaries available
 if ! command -v sed &> /dev/null
@@ -13,7 +12,6 @@ then
     exit 1
 fi
 
-# prepare the changelog file, put it in the liquibase changesets folder so it can be picked up
+# prepare the changelog file, put it in the liquibase changesets folder so it can be picked up by liquibase
 sed -E 's/\s*\-\s*\[pr.*//g' $changelogFile > $changesetFolder/$tempMdFileName
-docker run --rm --volume "`realpath $changesetFolder`:/data" --user `id -u`:`id -g` pandoc/minimal $tempMdFileName -o $publicChangeLogFileName
-#pandoc $tempMdFile -o $publicChangeLogFile --metadata title="Changelog Metadata Vlaanderen"
+docker run --rm --volume "$(realpath $changesetFolder):/data" --user "$(id -u)":"$(id -g)" pandoc/minimal $tempMdFileName -o $publicChangeLogFileName
