1c677b7a33cb10e7aa2a339bccc7e2c1ea7421ff
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2024-02-29 11:05:12 +0100

    testing changelog generation in pipeline

--- vlaanderen/scripts/public.changelog.sh
+++ vlaanderen/scripts/public.changelog.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-sed -E 's/\s*\-\s*\[pr.*//g' ../CHANGELOG.md > /tmp/public.changelog.md
-pandoc /tmp/public.changelog.md -o /tmp/public.changelog.html --metadata title="Changelog Metadata Vlaanderen"
+changesetFolder=../../liquibase/changesets
+tempMdFile=/tmp/public.changelog.md
+changelogFile=../CHANGELOG.md
+publicChangeLogFile=$changesetFolder/public.changelog.html
+
+# test we have the prerequisite binaries available
+if ! command -v pandoc &> /dev/null
+then
+    echo "pandoc could not be found"
+    exit 1
+fi
+if ! command -v sed &> /dev/null
+then
+    echo "sed could not be found"
+    exit 1
+fi
+
+# prepare the changelog file, put it in the liquibase changesets folder so it can be picked up
+sed -E 's/\s*\-\s*\[pr.*//g' $changelogFile > $tempMdFile
+pandoc $tempMdFile -o $publicChangeLogFile --metadata title="Changelog Metadata Vlaanderen"
