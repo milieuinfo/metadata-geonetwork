ac7665d71d493d833e6a3216a818b7008a814509
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2024-02-29 11:23:37 +0100

    testing pandoc in docker

--- vlaanderen/scripts/public.changelog.sh
+++ vlaanderen/scripts/public.changelog.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 changesetFolder=../../liquibase/changesets
-tempMdFile=/tmp/public.changelog.md
+tempMdFileName=public.changelog.md
+tempMdFile=/tmp/$tempMdFileName
 changelogFile=../CHANGELOG.md
-publicChangeLogFile=$changesetFolder/public.changelog.html
+publicChangeLogFileName=public.changelog.html
+publicChangeLogFile=$changesetFolder/$publicChangeLogFileName
 
 # test we have the prerequisite binaries available
-if ! command -v pandoc &> /dev/null
-then
-    echo "pandoc could not be found"
-    exit 1
-fi
 if ! command -v sed &> /dev/null
 then
     echo "sed could not be found"
@@ -17,5 +14,6 @@ then
 fi
 
 # prepare the changelog file, put it in the liquibase changesets folder so it can be picked up
-sed -E 's/\s*\-\s*\[pr.*//g' $changelogFile > $tempMdFile
-pandoc $tempMdFile -o $publicChangeLogFile --metadata title="Changelog Metadata Vlaanderen"
+sed -E 's/\s*\-\s*\[pr.*//g' $changelogFile > $changesetFolder/$tempMdFileName
+docker run --rm --volume "`realpath $changesetFolder`:/data" --user `id -u`:`id -g` pandoc/minimal $tempMdFileName -o $publicChangeLogFileName
+#pandoc $tempMdFile -o $publicChangeLogFile --metadata title="Changelog Metadata Vlaanderen"
